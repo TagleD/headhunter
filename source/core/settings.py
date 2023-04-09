807a17c0e231c61da5689001c22e35205c4fb096
yedige87 <yedabdygapparov@gmail.com>
2023-04-10 00:39:03 +0600

    #2 Обновить файл settings.py
    В файле settings.py переопределить кастомного юзера AUTH_USER_MODEL = 'accounts.Account' и в INSTALLED_APPS добавить 'accounts'

--- source/core/settings.py
+++ source/core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
