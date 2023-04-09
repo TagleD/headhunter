d77d68274788243970853760d727de4f7c905778
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-04-09 21:23:11 +0600

    #1 Настроен файл settings.py
    - Поменять в словаре DATABASES.ENGINE на django.db.backends.postgresql
    - Поменять DATABASES.NAME на headhunter
    - Поменять DATABASES.USER на postgres
    - Поменять DATABASES.PASSWORD на соответствующий
    - Поменять DATABASES.HOST на localhost
    - Поменять DATABASES.PORT на 5432
    - Поменять LANGUAGE_CODE на ru-ru

--- source/core/settings.py
+++ source/core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'headhunter',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
