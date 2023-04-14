06faa1de95fe8da1f873a0cec1d202ac765735e4
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-14 18:50:20 +0600

    #5 Создать модель Resume внутри приложения applicants
    Внутри приложения applicants удалить файл models.py,
    создать директорию models с файлом resume.py, где прописать модель Resume
    Создать миграцию для новой модели

---
+++ source/applicants/models/resume.py
@@ -0,0 +1,68 @@
+from django.conf import settings
+from django.db import models
+
+
+class Resume(models.Model):
+    applicant = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        verbose_name='Соискатель',
+        related_name='resumes',
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+    full_name = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False,
+        verbose_name='Полное имя'
+    )
+    position = models.CharField(
+        max_length=128,
+        null=False,
+        blank=False,
+        verbose_name='Должность'
+    )
+    salary = models.PositiveIntegerField(
+        max_length=8,
+        null=False,
+        blank=False,
+        verbose_name='Желаемая зарплата'
+    )
+    about = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False,
+        verbose_name='О себе'
+    )
+    telegram = models.CharField(
+        max_length=256,
+        null=False,
+        blank=False,
+        verbose_name='Nickname telegram'
+    )
+    facebook = models.CharField(
+        max_length=512,
+        null=True,
+        verbose_name='Facebook profile URL'
+    )
+    linkedin = models.CharField(
+        max_length=512,
+        null=True,
+        verbose_name='Linkedin profile URL'
+    )
+    is_published = models.BooleanField(
+        null=False,
+        blank=False,
+        default=True,
+        verbose_name='Статус публикации'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Дата обновления",
+        null=True
+    )
