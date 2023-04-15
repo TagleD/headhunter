c9b30c30d35e0d7cc4e1a6529793173490603198
GLeb Tarakanov <glebtgd@gmail.com>
2023-04-15 18:18:41 +0600

    #5 создать представление для просмотра личного кабинета соискателя
     Реализовать с помощью ListView, где в model указать Resume и в get_query_set добавить фильтр для того, чтобы выводились только те резюме, которые относятся к просматриваемому пользователю. Данные из модели Account (user_type, name, avatar) пробрасываем через get_context_data

--- source/applicants/views.py
+++ source/applicants/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+
+from accounts.models import Account
+from applicants.models import Resume
+
 
 # Create your views here.
+
+
+class ApplicantDetailView(ListView):
+    template_name = 'applicants/applicant_detail.html'
+    model = Resume
+    ordering = ('-created_at',)
+    paginate_orphans = 1
+    paginate_by = 4
+
+    def get_queryset(self):
+        applicant_id = self.kwargs.get('pk')
+        return Resume.objects.filter(applicant_id=applicant_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        applicant = Account.objects.get(pk=self.kwargs.get('pk'))
+        context['user_email'] = applicant.email
+        context['user_name'] = applicant.name
+        context['user_phone'] = applicant.phone
+        context['user_avatar'] = applicant.avatar
+        return context
